1b2961056246ddb6d7854cad2d9f2d042da7d940
fsalum <fsalum@gmail.com>
2014-02-14 20:27:47 -0800

    fixing base packages

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,10 +1,5 @@
 # Base install
 
-sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
-
-# Make ssh faster by not waiting on DNS
-echo "UseDNS no" >> /etc/ssh/sshd_config
-
 cat > /etc/yum.repos.d/epel.repo << EOM
 [epel]
 name=epel
@@ -12,3 +7,10 @@ baseurl=http://download.fedoraproject.org/pub/epel/6/\$basearch
 enabled=1
 gpgcheck=0
 EOM
+
+yum -y install openssh openssh-server openssh-clients sudo gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget dkms nfs-utils
+
+sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+
+# Make ssh faster by not waiting on DNS
+echo "UseDNS no" >> /etc/ssh/sshd_config
